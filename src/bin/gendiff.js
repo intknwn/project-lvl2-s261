ec49d5643b746241d9e11c16c92bf64fbdcb5f23
intknwn <intknwn@gmail.com>
2018-05-14 13:53:56 +0800

    Added basic functionality

---
+++ src/bin/gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import program from "commander";
+
+program
+  .version('0.0.1')
+  .option('-o, --option','option description')
+  .option('-m, --more','we can have as many options as we want')
+  .option('-i, --input [optional]','optional user input')
+  .option('-I, --another-input <required>','required user input')
+  .parse(process.argv);
+
+
+
