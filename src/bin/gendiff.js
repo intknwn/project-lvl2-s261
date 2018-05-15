d451c93b4ddf3cd34e42ac6c5167deddd6f1062b
intknwn <intknwn@gmail.com>
2018-05-15 12:49:43 +0800

    Fixing output

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -8,6 +8,6 @@ program
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
-  .action((firstConfig, secondConfig) => console.log(diff(firstConfig, secondConfig)));
+  .action(console.log((firstConfig, secondConfig) => diff(firstConfig, secondConfig)));
 
 program.parse(process.argv);
